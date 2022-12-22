fa87933f428e7c4463c29d8b6d371e24cf83ea3d
Fasterplop <fasterplop@gmail.com>
2022-12-22 16:12:59 -0500

    Commits to dario to fix his life

--- src/components/ui/Dropdown/DropdownItem.tsx
+++ src/components/ui/Dropdown/DropdownItem.tsx
@@ -1,33 +1,33 @@
-import { Menu } from '@headlessui/react';
-import classNames from 'classnames';
-import * as React from 'react';
+import { Menu } from "@headlessui/react";
+import classNames from "classnames";
+import * as React from "react";
 
 interface IDropdownItemProps {
-	children?: React.ReactNode;
-	className?: string;
-	classNameIsActive?: string;
-	onClick?(): void;
+  children?: React.ReactNode;
+  className?: string;
+  classNameIsActive?: string;
+  onClick?(): void;
 }
 
-const DropdownItem: React.FunctionComponent<IDropdownItemProps> = props => {
-	const { className, classNameIsActive, onClick } = props;
-	const spanClassName = (active: boolean) => {
-		return classNames(
-			'block px-4 py-2 text-sm cursor-pointer',
-			{ 'bg-gray-100 text-gray-900': active, 'text-gray-700': !active },
-			classNameIsActive && active ? classNameIsActive : '',
-			className
-		);
-	};
-	return (
-		<Menu.Item>
-			{({ active }) => (
-				<span className={spanClassName(active)} onClick={onClick}>
-					{props.children}
-				</span>
-			)}
-		</Menu.Item>
-	);
+const DropdownItem: React.FunctionComponent<IDropdownItemProps> = (props) => {
+  const { className, classNameIsActive, onClick } = props;
+  const spanClassName = (active: boolean) => {
+    return classNames(
+      "block px-4 py-2 text-sm cursor-pointer",
+      { "bg-gray-100": active, "text-gray-700": !active },
+      classNameIsActive && active ? classNameIsActive : "",
+      className
+    );
+  };
+  return (
+    <Menu.Item>
+      {({ active }) => (
+        <span className={spanClassName(active)} onClick={onClick}>
+          {props.children}
+        </span>
+      )}
+    </Menu.Item>
+  );
 };
 
 export default DropdownItem;
--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -1,314 +1,225 @@
-import { Alert, Checkbox, Drawer, Dropdown, DropdownItem, Switch, Tooltip } from '@/components/ui';
-import * as React from 'react';
-import AuthSide from './AuthSide';
-import SignInForm from './SignInForm';
+import {
+  Alert,
+  Checkbox,
+  Drawer,
+  Dropdown,
+  DropdownItem,
+  Switch,
+  Tooltip,
+} from "@/components/ui";
+import * as React from "react";
+import AuthSide from "./AuthSide";
+import SignInForm from "./SignInForm";
 
 interface IAuthLayoutProps {}
 
-const AuthLayout: React.FunctionComponent<IAuthLayoutProps> = props => {
-	const [isOpen, setIsOpen] = React.useState(false);
-	const [isChecked, setIsChecked] = React.useState(false);
-	const [enabled, setEnabled] = React.useState(false);
-	return (
-		<div className="app-layout-blank flex flex-auto flex-col h-[100vh]">
-			<AuthSide>
-				<div className="mb-8">
-					<h3 className="mb-1">Welcome back!</h3>
-					<p>Please enter your credentials to sign in!</p>
-				</div>
-				{/* 			<div>
-					<Alert type="info" title={'Oops!'}>
-						Ha ocurrido un error en el servidor
-					</Alert>
+const AuthLayout: React.FunctionComponent<IAuthLayoutProps> = (props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isChecked, setIsChecked] = React.useState(false);
+  const [enabled, setEnabled] = React.useState(false);
 
-					<Alert type="danger" className="mt-2" title={'Oops!'}>
-						Ha ocurrido un error en el servidor
-					</Alert>
+  return (
+    <div className="app-layout-blank flex flex-auto flex-col h-[100vh]">
+      <AuthSide>
+        <div className="mb-8">
+          <h3 className="mb-1">Welcome back!</h3>
+          <p>Please enter your credentials to sign in!</p>
+        </div>
+        {/* Drawer{" "}
+        <div>
+          <button className="btn btn-primary" onClick={() => setIsOpen(true)}>
+            Open Drawer
+          </button>
+          <Drawer
+            isOpen={isOpen}
+            onClose={() => setIsOpen(false)}
+            footer={<h1>sex</h1>}
+            title="SEX"
+            placement="right"
+          ></Drawer>
+        </div> */}
+        {/* <div>
+          <Dropdown
+            displayButton={
+              <button className="btn btn-alternative btn-sm">Options</button>
+            }
+          >
+            <DropdownItem
+              onClick={() => null}
+              className=" bg-red-500 text-white"
+            >
+              Account settings
+            </DropdownItem>
+            <DropdownItem>Support</DropdownItem>
+            <DropdownItem>License</DropdownItem>
+            <DropdownItem>Sign out</DropdownItem>
+          </Dropdown>
+        </div> */}
+        {/* <div>
+          <Tooltip message={"Hello World!"}>
+            <button className="btn btn-primary">Hover me Please!</button>
+          </Tooltip>
+          <Tooltip message={"Whatssup you!"}>
+            <button className="ml-3 btn btn-warning">
+              Dont hover me, i'm scared!
+            </button>
+          </Tooltip>
+        </div> */}
 
-					<Alert type="success" className="mt-2" title={'Oops!'}>
-						Ha ocurrido un error en el servidor
-					</Alert>
+        <div>
+          <h6 className="mb-6 text-center">DEFAULT INPUT</h6>
+          <div className="form-group">
+            <label htmlFor="floating_email">Email address</label>
 
-					<Alert type="warning" className="mt-2" title={'Oops!'}>
-						Ha ocurrido un error en el servidor
-					</Alert>
-				</div> */}
-				{/* 	BOTONES			<div>
-					<h5 className="mb-4 text-center">Default Buttons</h5>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+            />
 
-					<button className="btn btn-primary">Default</button>
-					<button className="btn btn-alternative">Alternative</button>
-					<button className="btn btn-dark">Dark</button>
-					<button className="btn btn-light">Light</button>
+            <p>Please fill this input</p>
+          </div>
 
-					<button className="btn btn-success">Success</button>
-					<button className="btn btn-danger">Danger</button>
-					<button className="btn btn-warning">Warning</button>
+          <div className="form-group form-group-success">
+            <label htmlFor="floating_email">Email address</label>
 
-					<hr className="my-4" />
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+            />
 
-					<h5 className="mb-4 text-center">Pill Buttons</h5>
+            <p>Alright! Username available!</p>
+          </div>
 
-					<button className="btn btn-pill btn-primary">
-						Default
-					</button>
-					<button className="btn btn-pill btn-alternative">
-						Alternative
-					</button>
-					<button className="btn btn-pill btn-dark">Dark</button>
-					<button className="btn btn-pill btn-light">Light</button>
+          <div className="form-group form-group-error">
+            <label htmlFor="floating_email">Email address</label>
 
-					<button className="btn btn-pill btn-success">
-						Success
-					</button>
-					<button className="btn btn-pill btn-danger">Danger</button>
-					<button className="btn btn-pill btn-warning">
-						Warning
-					</button>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+            />
 
-					<hr className="my-4" />
+            <p>Oops! Username already taken!</p>
+          </div>
 
-					<h5 className="mb-4 text-center">Disabled Buttons</h5>
+          <h6 className="mb-6 text-center">SIZES</h6>
+          <hr className="mb-3" />
 
-					<button disabled className="btn btn-primary">
-						Default
-					</button>
-					<button disabled className="btn btn-alternative">
-						Alternative
-					</button>
-					<button disabled className="btn btn-dark">
-						Dark
-					</button>
-					<button disabled className="btn btn-light">
-						Light
-					</button>
+          <div className="form-group">
+            <label htmlFor="floating_email">SM</label>
 
-					<button disabled className="btn btn-success">
-						Success
-					</button>
-					<button disabled className="btn btn-danger">
-						Danger
-					</button>
-					<button disabled className="btn btn-warning">
-						Warning
-					</button>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+              className="form-control-sm"
+            />
+          </div>
 
-					<hr className="my-4" />
+          <div className="form-group">
+            <label htmlFor="floating_email">Base</label>
 
-					<h5 className="mb-4 text-center">Sizes</h5>
-					<button className="btn btn-xs btn-primary">XS</button>
-					<button className="btn btn-sm btn-primary">SM</button>
-					<button className="btn btn-md btn-primary">MD</button>
-					<button className="btn btn-lg btn-primary">LG</button>
-					<button className="btn btn-xl btn-primary">XL</button>
-				</div> */}
-				{/* TYPOGRAPHY		<div>
-					<h1 className="text-center mb-4">Typography</h1>
-					<h1>h1</h1>
-					<h2>h2</h2>
-					<h3>h3</h3>
-					<h4>h4</h4>
-					<h5>h5</h5>
-					<h6>h6</h6>
-				</div> */}
-				{/* 				Drawer{' '}
-				<div>
-					<button
-						className="btn btn-primary"
-						onClick={() => setIsOpen(true)}
-					>
-						Open Drawer
-					</button>
-					<Drawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
-				</div> */}
-				{/* 			<div>
-					<Dropdown
-						displayButton={
-							<button className="btn btn-alternative btn-sm">
-								Options
-							</button>
-						}
-					>
-						<DropdownItem>Account settings</DropdownItem>
-						<DropdownItem>Support</DropdownItem>
-						<DropdownItem>License</DropdownItem>
-						<DropdownItem>Sign out</DropdownItem>
-					</Dropdown>
-				</div> */}
-				{/* 				<div>
-					<Tooltip message={'Hello World!'}>
-						<button className="btn btn-primary">
-							Hover me Please!
-						</button>
-					</Tooltip>
-					<Tooltip message={'Whatssup you!'}>
-						<button className="ml-3 btn btn-warning">
-							Dont hover me, i'm scared!
-						</button>
-					</Tooltip>
-				</div> */}
-				{/* 
-				<div>
-					<h6 className="mb-6 text-center">DEFAULT INPUT</h6>
-					<div className="form-group">
-						<label htmlFor="floating_email">
-							Email address
-						</label>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="floating_email">Large</label>
 
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-						/>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              placeholder=" "
+              required
+              className="form-control-lg"
+            />
+          </div>
+        </div>
 
-						<p>Please fill this input</p>
-					</div>
+        <h6 className="mb-6 text-center">DISABLED</h6>
+        <div className="form-group">
+          <label htmlFor="floating_email">Email address</label>
 
-					<div className="form-group form-group-success">
-						<label htmlFor="floating_email">
-							Email address
-						</label>
+          <input
+            type="text"
+            name="floating_email"
+            id="floating_email"
+            placeholder=" "
+            required
+            disabled
+          />
+        </div>
 
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-						/>
+        <div className="form-group">
+          <textarea
+            id="message"
+            rows={4}
+            placeholder="Leave a comment..."
+          ></textarea>
+        </div>
 
-						<p>Alright! Username available!</p>
-					</div>
+        <div className="form-group">
+          <select>
+            <option>United States</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+        </div>
 
-					<div className="form-group form-group-error">
-						<label htmlFor="floating_email">
-							Email address
-						</label>
+        <div className="form-group">
+          <select className="form-control-lg">
+            <option>United States</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+        </div>
 
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-						/>
+        <div className="form-group">
+          <select className="form-control-sm">
+            <option>United States</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+        </div>
 
-						<p>Oops! Username already taken!</p>
-					</div>
+        <div className="form-group">
+          <Checkbox isChecked={isChecked} onChange={setIsChecked}>
+            I agree to the{" "}
+            <a
+              href="#"
+              className="text-blue-600 hover:underline dark:text-blue-500"
+            >
+              terms and condition
+            </a>
+            .
+          </Checkbox>
+        </div>
 
-					<h6 className="mb-6 text-center">SIZES</h6>
-					<hr className="mb-3" />
-
-					<div className="form-group">
-						<label htmlFor="floating_email">SM</label>
-
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-							className="form-control-sm"
-						/>
-					</div>
-
-					<div className="form-group">
-						<label htmlFor="floating_email">Base</label>
-
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<label htmlFor="floating_email">Large</label>
-
-						<input
-							type="text"
-							name="floating_email"
-							id="floating_email"
-							placeholder=" "
-							required
-							className="form-control-lg"
-						/>
-					</div>
-				</div>
-
-				<h6 className="mb-6 text-center">DISABLED</h6>
-				<div className="form-group">
-					<label htmlFor="floating_email">Email address</label>
-
-					<input
-						type="text"
-						name="floating_email"
-						id="floating_email"
-						placeholder=" "
-						required
-						disabled
-					/>
-				</div>
-
-				<div className="form-group">
-					<textarea
-						id="message"
-						rows={4}
-						placeholder="Leave a comment..."
-					></textarea>
-				</div>
-
-				<div className="form-group">
-					<select>
-						<option>United States</option>
-						<option>Canada</option>
-						<option>France</option>
-						<option>Germany</option>
-					</select>
-				</div>
-
-				<div className="form-group">
-					<select className="form-control-lg">
-						<option>United States</option>
-						<option>Canada</option>
-						<option>France</option>
-						<option>Germany</option>
-					</select>
-				</div>
-
-				<div className="form-group">
-					<select className="form-control-sm">
-						<option>United States</option>
-						<option>Canada</option>
-						<option>France</option>
-						<option>Germany</option>
-					</select>
-				</div>
-
-				<div className="form-group">
-					<Checkbox isChecked={isChecked} onChange={setIsChecked}>
-						I agree to the{' '}
-						<a
-							href="#"
-							className="text-blue-600 hover:underline dark:text-blue-500"
-						>
-							terms and condition
-						</a>
-						.
-					</Checkbox>
-				</div>
-
-				<div className="form-group">
-					<Switch isChecked={enabled} onChange={setEnabled} />
-				</div> */}
-
-				<SignInForm />
-			</AuthSide>
-		</div>
-	);
+        <div className="form-group">
+          <Switch isChecked={enabled} onChange={setEnabled} />
+        </div>
+        <SignInForm />
+      </AuthSide>
+    </div>
+  );
 };
 
 export default AuthLayout;
